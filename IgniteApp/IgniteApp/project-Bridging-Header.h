998337d8ab57d28067c75804879970f06464e5cf
Aditya Dhingra <adityadhingra07@gmail.com>
2016-11-11 10:51:30 -0500

    Adding PodFile, added Firebase, Google sign-n

---
+++ IgniteApp/IgniteApp/project-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  project-Bridging-Header.h
+//  IgniteApp
+//
+//  Created by Aditya Dhingra on 11/11/16.
+//  Copyright © 2016 Aditya Dhingra. All rights reserved.
+//
+
+#ifndef project_Bridging_Header_h
+#define project_Bridging_Header_h
+
+#import <GoogleSignIn/GoogleSignIn.h>
+
+#endif /* project_Bridging_Header_h */
